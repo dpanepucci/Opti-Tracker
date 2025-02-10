59c3d4543a1ead3f45f5b7a9aabbad6bb17b8563
Dylan Panepucci <dylanpanepucci@Mac.lan>
2025-02-10 13:27:30 -0500

    updated schema and seeds for snytax errors. added content to env file

--- db/schema.sql
+++ db/schema.sql
@@ -8,22 +8,26 @@ CREATE TABLE department (
     department_name VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE role (
-id SERIAL PRIMARY KEY, 
-title VARCHAR(255),
-salary DECIMAL,
-FOREIGN KEY (department_id)
-REFERENCES department(id)
-ON DELETE SET NULL 
+CREATE TABLE roles (
+    id SERIAL PRIMARY KEY, 
+    title VARCHAR(255) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT, 
+    FOREIGN KEY (department_id)
+        REFERENCES department(id)
+        ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-id SERIAL PRIMARY KEY, 
-first_name VARCHAR(255),
-last_name VARCHAR(255),
-FOREIGN KEY (role_id)
-REFERENCES role(id)
-ON DELETE SET NULL
-
-manager_id id given when manager is selected for role
+    id SERIAL PRIMARY KEY, 
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    roles_id INT, 
+    manager_id INT, -- Self-referencing column to indicate the employee's manager
+    FOREIGN KEY (roles_id)
+        REFERENCES roles(id)
+        ON DELETE SET NULL,
+    FOREIGN KEY (manager_id) -- References another employee as a manager
+        REFERENCES employee(id)
+        ON DELETE SET NULL
 );
